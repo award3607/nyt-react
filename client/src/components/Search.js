8c81d9adcc4081ee93a0c2a983ee457e9ead6b0b
Andrew Ward <andrew.rampant@gmail.com>
2018-02-18 16:12:34 -0600

    Search form component

---
+++ client/src/components/Search.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Search = props => {
+    <form>
+        <div className="form-group">
+            <label htmlFor="term">Search Term:</label>
+            <input 
+                onChange={props.handleInputChange}
+                value={props.term}
+                name="term"
+                type="text"
+                className="form-control"
+                id="term"
+            />
+            <label htmlFor="startYear">Starting Year:</label>
+            <input
+                onChange={props.handleInputChange}
+                value={props.startYear}
+                name="startYear"
+                type="text"
+                className="form-control"
+                id="startYear"
+            />
+            <label htmlFor="endYear">Ending Year:</label>
+            <input
+                onChange={props.handleInputChange}
+                value={props.endYear}
+                name="endYear"
+                type="text"
+                className="form-control"
+                id="endYear"
+            />
+            <button onClick={props.handleSearchSubmit} className="btn btn-primary">
+                Search
+            </button>
+        </div>
+    </form>
+};
+
+export default Search;
